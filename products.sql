0641f056d167c9394bad18421507e1504ab1f908
rxi16 <rxi16@users.noreply.github.com>
2017-10-12 13:41:23 -0400

    Add files via upload

--- products.sql
+++ products.sql
@@ -11,7 +11,7 @@ CREATE TABLE products (
   PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, price, stock_quantity) VALUES ('Microbiology', 'books', 62.75, 100), ('Behavioral Statistics', 'books', 41.35, 245), ('On Intelligence', 'books', 19.99, 35),
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Microbiology', 'books', 62.75, 100), ('Behavioral Statistics', 'books', 41.35, 245), ('On Intelligence', 'books', 19.99, 35),
 ('vitamin D 100 count', 'supplements', 14.99, 40), ('calcium carbonate 100 count', 'supplements', 7.99, 85), ('calcium citrate 100 count', 'supplements', 9.99, 85), ('dates 12 oz', 'groceries', 5.95, 25), 
 ('raisins 12 oz', 'groceries', 5.95, 20), ('goji berries 12 oz', 'groceries', 15.95, 25), ('sterling silver chain bracelet', 'jewelry', 136.00, 2);
 
